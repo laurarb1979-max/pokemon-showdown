fe07932d65969f1a8ee45c7b7aad83d3744e8d76
laurarb1979-max <laurarb1979@gmail.com>
2026-09-17 19:10:59 +0200

    Crear mods/fakemon/pokedex.ts

---
+++ data/mods/fakemon/pokedex.ts
@@ -0,0 +1,9 @@
+export const pokedex: {[k string]: ModdedSpeciesData} = {
+legomanster: {
+num: -614,
+name: "Legomanster",
+baseSpecies: "Charizard-Mega-Y"
+types: ["Dragon","Fire"],
+baseStats: {hp: 100, atk: 50, def: 115, spa: 120, spd: 70, spe: 79},
+abilities: {0: "Flash Fire", H: "Drought"},
+weightkg: 349,
